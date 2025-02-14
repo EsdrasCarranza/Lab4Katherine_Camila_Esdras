4f05b0027a7b7774d9714f4712ae2fa6123bacd1
camilaflores07 <camila_maria@unitec.edu>
2025-02-14 14:28:18 -0600

    logica de la clase ahorcado fijo

--- src/lab4esdras_kathe_camila/JuegoAhorcadoBase.java
+++ src/lab4esdras_kathe_camila/JuegoAhorcadoBase.java
@@ -9,17 +9,17 @@ package lab4esdras_kathe_camila;
  * @author 50488
  */
 public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
-     String PalabraSecreta, Palabractual;
+     String PalabraSecretaFija, Palabractual;
      int intentosRestantes;
 
     public JuegoAhorcadoBase(String PalabraSecreta, String Palabractual, int intentosRestantes) {
-        this.PalabraSecreta = PalabraSecreta;
+        this.PalabraSecretaFija = PalabraSecretaFija;
         this.Palabractual = "_".repeat(PalabraSecreta.length());
         this.intentosRestantes = 10;
     }
     
     public abstract char actualizarPalabraActual(char letra);
-    public abstract char verificarLetra(char letra);
+    public abstract boolean verificarLetra(char letra);
     public abstract boolean hasGanado();
     
     
--- src/lab4esdras_kathe_camila/JuegoAhorcadoFijo.java
+++ src/lab4esdras_kathe_camila/JuegoAhorcadoFijo.java
@@ -4,6 +4,8 @@
  */
 package lab4esdras_kathe_camila;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 50488
@@ -20,31 +22,65 @@ public class JuegoAhorcadoFijo extends JuegoAhorcadoBase {
 
     @Override
     public char actualizarPalabraActual(char letra) {
+       String nuevaPalabraActual = "";
+        for (int i = 0; i < PalabraSecretaFija.length(); i++) {
+            if (PalabraSecretaFija.charAt(i) == letra) {
+                nuevaPalabraActual += letra; 
+            } else {
+                nuevaPalabraActual += Palabractual.charAt(i); 
+            }
+        }
+        Palabractual = nuevaPalabraActual;
         return 0;
-    
     }
-
+   
     @Override
-    public char verificarLetra(char letra) {
-        return 0;
+    public boolean verificarLetra(char letra) {
+    boolean PalabraCorrecta = PalabraSecretaFija.indexOf(letra) >= 0;
+        if (PalabraCorrecta) {
+            actualizarPalabraActual(letra);
+        } else {
+            intentosRestantes--;
+        }
+        return PalabraCorrecta;
     }
 
     @Override
     public boolean hasGanado() {
-      return true;
-    }
-
-    @Override
-    public void inicializarPalabraSecreta() {
-      
+     return Palabractual.equals(PalabraSecretaFija);
     }
 
     @Override
     public void jugar() {
+        JOptionPane.showMessageDialog(null, "HORA DE EMPEZAR \nAdivine la palabra");
+        while (!hasGanado() && intentosRestantes > 0) {
+            String letra=" ";
+            letra = JOptionPane.showInputDialog(null, "Palabra: " + Palabractual
+                    + "\nIntentos: " + intentosRestantes
+                    + "\nIngresa una letra:");
+            if (letra == null || letra.isEmpty()) {
+                break;
+            }else{ 
+                letra=letra.toUpperCase();
+                char letraChar = letra.charAt(0);
+                if (verificarLetra(letraChar)) {
+                    actualizarPalabraActual(letraChar);
+                    if (hasGanado()) {
+                        JOptionPane.showMessageDialog(null, "FELICIDADES USTED GANO! Su palabra era: " + intentosRestantes);
+                    }
+                } else {
+                    if (intentosRestantes == 0) {
+                        JOptionPane.showMessageDialog(null, "PERDEDOR! La palabra era: " + intentosRestantes);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "OH NO, Letra incorrecta, por favor intenta de nuevo.");
+                    }
+                }
+            }
+        }
     }
 
-   
-
-    
-    
+    @Override
+    public void inicializarPalabraSecreta() {
+        System.out.println("La palabra secreta es: " + PalabraSecretaFija);
+    }   
 }
